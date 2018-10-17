fae82e364c603be89b418849e3d66715efc31dc5
TJ <tarunjha@outlook.com>
2018-10-17 16:41:33 +0530

    Code files at the end of Chapter 10 LE JavaScript

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
-let Phrase = require("mhartl-palindrome");
-let string = prompt("Please enter a string for palindrome testing:");
-let phrase = new Phrase(string);
+let Phrase = require("mhartl-palindrome");// a published module which checks for palindrome 
 
-if (phrase.palindrome()) {
-  alert(`"${phrase.content}" is a palindrome!`);
-} else {
-  alert(`"${phrase.content}" is not a palindrome.`)
+
+function palindromeTester() {
+  let string = prompt("Please enter a string for palindrome testing:");
+  let phrase = new Phrase(string);
+  console.log("Input was"+ phrase);
+  console.log("input was"+string);
+    let palindromeResult = document.querySelector("#palindromeResult");
+    console.log("where is palindromeResult"+ palindromeResult);
+  if (phrase.palindrome()) {
+    palindromeResult.innerHTML = `"${phrase.content}" is a palindrome!`;
+  } else {
+    palindromeResult.innerHTML = `"${phrase.content}" is not a palindrome.`;
+  }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  let button = document.querySelector("#palindromeTester");
+  console.log ("button is"+ button);
+  button.addEventListener("click", function() {
+    palindromeTester();
+  });
+});
